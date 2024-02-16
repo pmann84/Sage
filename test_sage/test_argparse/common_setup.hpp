5729b95ab316a878205fa812afe644193b0c0146
pmann84 <peterdmann28@gmail.com>
2024-02-16 12:44:40 +0000

    Missed file in last commit

---
+++ test_sage/test_argparse/common_setup.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+inline char AppName[] = "DummyApp.exe";
+inline char BarArgName[] = "bar";
+inline char BarOptArgName[] = "--bar";
+inline char BarShortOptArgName[] = "-b";
+inline char BarArgValue[] = "BAR";
+inline char BarArgValue1[] = "BAR1";
+inline char BarArgValue2[] = "BAR2";
+inline char BarHelp[] = "Positional bar argument.";
+inline char FooArgName[] = "foo";
+inline char FooOptArgName[] = "--foo";
+inline char FooShortOptArgName[] = "-f";
+inline char FooArgValue[] = "FOO";
+inline char FooArgValue1[] = "FOO1";
+inline char FooArgValue2[] = "FOO2";
+inline char FooArgValue3[] = "FOO3";
+inline char FooArgValue4[] = "FOO4";
+inline char FooArgValue5[] = "FOO5";
+inline char FooHelp[] = "Positional foo argument.";
+inline char FooOptHelp[] = "Optional Foo argument.";
+inline char BazArgValue[] = "BAZ";
