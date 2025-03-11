01654d83f027d27adc184e53c71b1329e0a265e4
pmann84 <peterdmann28@gmail.com>
2025-03-11 12:06:33 +0000

    Expanded monitor diagnostics

--- sage/include/sage/performance/monitors.hpp
+++ sage/include/sage/performance/monitors.hpp
@@ -36,12 +36,25 @@ namespace sage::performance
             return m_measurements;
         }
 
+        [[nodiscard]] double average() const {
+            return total() / m_measurements.size();
+        }
+
         [[nodiscard]] double total() const {
             return std::accumulate(m_measurements.begin(), m_measurements.end(), 0.0);
         }
 
         [[nodiscard]] std::string s_total() const {
-            auto ms = std::chrono::duration<double>(total() * 0.001);
+            return format_time(total());
+        }
+
+        [[nodiscard]] std::string s_average() const {
+            return format_time(average());
+        }
+
+    private:
+        static std::string format_time(double duration_in_ms) {
+            auto ms = std::chrono::duration<double>(duration_in_ms * 0.001);
             auto secs = std::chrono::duration_cast<std::chrono::seconds>(ms);
             ms -= std::chrono::duration_cast<std::chrono::milliseconds>(secs);
             auto mins = std::chrono::duration_cast<std::chrono::minutes>(secs);
@@ -50,7 +63,7 @@ namespace sage::performance
             mins -= std::chrono::duration_cast<std::chrono::minutes>(hour);
 
             std::stringstream ss;
-            ss << hour.count() << " Hours : " << mins.count() << " Minutes : " << secs.count() << " Seconds : " << ms.count() << " Milliseconds";
+            ss << std::setw(2) << std::setfill('0') << hour.count() << ":" <<std::setw(2) << std::setfill('0') <<  mins.count() << ":" << std::setw(2) << std::setfill('0') << secs.count() << ":" << std::chrono::duration_cast<std::chrono::milliseconds>(ms).count();
             return ss.str();
         }
 
--- test_sage/test_performance/monitors_tests.cpp
+++ test_sage/test_performance/monitors_tests.cpp
@@ -27,3 +27,39 @@ TEST(PerformanceMonitorTests, TestPerformanceMonitorTotalIsCorrect)
 
     ASSERT_THAT(perf_monitor.total(), testing::DoubleEq(1500));
 }
+
+TEST(PerformanceMonitorTests, TestPerformanceMonitorSTotalIsCorrect)
+{
+    sage::performance::performance_monitor perf_monitor;
+    perf_monitor.add_measurement(std::chrono::milliseconds(100));
+    perf_monitor.add_measurement(std::chrono::milliseconds(200));
+    perf_monitor.add_measurement(std::chrono::milliseconds(300));
+    perf_monitor.add_measurement(std::chrono::milliseconds(400));
+    perf_monitor.add_measurement(std::chrono::milliseconds(500));
+
+    ASSERT_THAT(perf_monitor.s_total(), testing::StrEq("00:00:01:500"));
+}
+
+TEST(PerformanceMonitorTests, TestPerformanceMonitorAverageIsCorrect)
+{
+    sage::performance::performance_monitor perf_monitor;
+    perf_monitor.add_measurement(std::chrono::milliseconds(100));
+    perf_monitor.add_measurement(std::chrono::milliseconds(200));
+    perf_monitor.add_measurement(std::chrono::milliseconds(300));
+    perf_monitor.add_measurement(std::chrono::milliseconds(400));
+    perf_monitor.add_measurement(std::chrono::milliseconds(500));
+
+    ASSERT_THAT(perf_monitor.average(), testing::DoubleEq(300));
+}
+
+TEST(PerformanceMonitorTests, TestPerformanceMonitorSAverageIsCorrect)
+{
+    sage::performance::performance_monitor perf_monitor;
+    perf_monitor.add_measurement(std::chrono::milliseconds(100));
+    perf_monitor.add_measurement(std::chrono::milliseconds(200));
+    perf_monitor.add_measurement(std::chrono::milliseconds(300));
+    perf_monitor.add_measurement(std::chrono::milliseconds(400));
+    perf_monitor.add_measurement(std::chrono::milliseconds(500));
+
+    ASSERT_THAT(perf_monitor.s_average(), testing::StrEq("00:00:00:300"));
+}
